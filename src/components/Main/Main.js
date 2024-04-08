476dfa185b4dd293fa27f5d51b2040397c81959d
DamirXolboyev <104071903+DamirXolboyev@users.noreply.github.com>
2024-04-08 08:03:56 +0500

    main

---
+++ src/components/Main/Main.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import rasm1 from '../../components/img/adir-Cargo-Insurance 1.png';
+import './Main.css'
+
+
+const data =[
+    {
+        id: 1,
+        title: 'Поставщик Дальнего Востока',
+        h3: 'Узнать больше'
+    }
+];
+const Main = () => {
+    return (
+        <div className="hero-tim">
+            {data.map((item) => (
+                <div key={item.id} className="hero container">
+                    <div className="hero-logo">
+                        <h1>{item.title}</h1>
+                        <p>Наши клиенты получают полный спектр услуг по работе с Китаем. Начиная с подбора производителя заканчивая доставкой товара до двери. Настолько прозрачных сделок с Китаем вы еще не осуществляли.</p>
+                        <h3>{item.h3}</h3>
+                        <div className="all5"></div>
+                    </div>
+                    <div className="hero-logo">
+                        <img src={rasm1} alt="" />
+                    </div>
+                </div>
+            ))}
+        </div>
+    );
+}
+
+export default Main;
